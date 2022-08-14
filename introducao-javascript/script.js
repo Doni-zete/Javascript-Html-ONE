2b51ec20e9815b30b7b3542d62704a5af702c6ca
Donizete Crisostomo <donizete112@gmail.com>
2022-08-13 23:24:58 -0300

    imc no console

--- introducao-javascript/script.js
+++ introducao-javascript/script.js
@@ -1,4 +1,19 @@
 
-			let titulo = document.querySelector(".titulo");
-			console.log(titulo);
-			console.log("titulo.textContent");
+// let titulo = document.querySelector(".titulo");
+// console.log(titulo);
+// console.log("titulo.textContent");
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura =tdAltura.textContent;
+
+let imc = peso / (altura*altura);
+
+console.log(imc);
+console.log(tdAltura);
+console.log(tdPeso);
+console.log(paciente);

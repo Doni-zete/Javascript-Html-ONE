fb4546ab090087d4f8fba064dd5b4c08306fa854
Donizete Crisostomo <donizete112@gmail.com>
2022-08-15 19:47:22 -0300

    quebrando o codigo

---
+++ introducao-javascript/calcula-imc.js
@@ -0,0 +1,39 @@
+let titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+let pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+
+    let paciente = pacientes[i];
+
+    let tdPeso = paciente.querySelector(".info-peso");
+    let peso = tdPeso.textContent;
+
+    let tdAltura = paciente.querySelector(".info-altura");
+    let altura = tdAltura.textContent;
+
+    let tdImc = paciente.querySelector(".info-imc");
+
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        let imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}

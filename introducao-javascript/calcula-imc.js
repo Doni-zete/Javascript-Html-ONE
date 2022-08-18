208f9c036b67c552f424ef5fd3ab932d30f1a3ed
Donizete Crisostomo <donizete112@gmail.com>
2022-08-17 22:18:30 -0300

    refatorando

--- introducao-javascript/calcula-imc.js
+++ introducao-javascript/calcula-imc.js
@@ -1,31 +1,31 @@
-let titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-let pacientes = document.querySelectorAll(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-for (let i = 0; i < pacientes.length; i++) {
+for (var i = 0; i < pacientes.length; i++) {
 
-    let paciente = pacientes[i];
+    var paciente = pacientes[i];
 
-    let tdPeso = paciente.querySelector(".info-peso");
-    let peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-    let tdAltura = paciente.querySelector(".info-altura");
-    let altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-    let tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-    let pesoEhValido = true;
-    let alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.00) {
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
@@ -33,14 +33,32 @@ for (let i = 0; i < pacientes.length; i++) {
     }
 
     if (pesoEhValido && alturaEhValida) {
-        let imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
 }
 
-
 function calculaImc(peso, altura) {
-    let imc = 0;
+    var imc = 0;
     imc = peso / (altura * altura);
+
     return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+
+    if (peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
 }
